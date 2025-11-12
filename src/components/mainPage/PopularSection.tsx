8ef1094d52e7c9b936571e9b2a4332ddcbd55660
stickfig8 <gusrb0482@gmail.com>
2025-11-13 00:58:32 +0900

    feat: 인기목록 길이 0 예외 추가

--- src/components/mainPage/PopularSection.tsx
+++ src/components/mainPage/PopularSection.tsx
@@ -15,7 +15,7 @@ const PopularSection = () => {
   const { index, handleNext, handlePrev } = useSlideIndex(slides);
 
   return (
-    <section className="relative flex flex-col gap-[24px] w-full h-[527px] py-[36px]">
+    <section className="relative flex flex-col gap-[24px] w-full h-fit py-[36px]">
       {/* 텍스트 div */}
       <div className="flex justify-between items-center w-full h-[51px]">
         <div className="flex gap-[10px] w-fit h-full items-center">
@@ -30,45 +30,52 @@ const PopularSection = () => {
         </Link>
       </div>
 
-      {/* 버튼 포지션 용도로 카드 컨테이너 한번더 감쌈 */}
-      <div className="relative w-full h-[380px]">
-        {/* 카드 컨테이너 div */}
-        <div className="w-full h-[380px] mx-auto overflow-hidden ">
-          <div
-            className="flex transition-transform duration-700 ease-in-out gap-[20px]"
-            style={{
-              transform: `translateX(-${index * (1200 + 20)}px)`,
-              width: `${1200 * slides.length + 20 * (slides.length - 1)}px`,
-            }}
-          >
-            {slides.map((slide, i) => (
-              <PopularSlide key={i} placeList={slide} />
-            ))}
-          </div>
+      {resultList.length < 1 ? (
+        <div className="w-full h-[160px]">
+          <p className="text-center text-[14px] text-[var(--place-neutral)] py-8">
+            집계된 장소가 없습니다.
+          </p>
         </div>
+      ) : (
+        <div className="relative w-full h-[380px]">
+          {/* 카드 컨테이너 div */}
+          <div className="w-full h-[380px] mx-auto overflow-hidden ">
+            <div
+              className="flex transition-transform duration-700 ease-in-out gap-[20px]"
+              style={{
+                transform: `translateX(-${index * (1200 + 20)}px)`,
+                width: `${1200 * slides.length + 20 * (slides.length - 1)}px`,
+              }}
+            >
+              {slides.map((slide, i) => (
+                <PopularSlide key={i} placeList={slide} />
+              ))}
+            </div>
+          </div>
 
-        {/* 이동 버튼 */}
-        <button
-          className="absolute left-0 -translate-x-1/2 top-1/2 -translate-y-1/2 w-[40px] h-[40px] cursor-pointer"
-          onClick={handlePrev}
-        >
-          <img
-            src="/assets/buttons/button_left.png"
-            alt="left"
-            className="w-full h-full transition hover:brightness-95 active:brightness-92"
-          />
-        </button>
-        <button
-          className="absolute right-0 translate-x-1/2 top-1/2 -translate-y-1/2 w-[40px] h-[40px] cursor-pointer "
-          onClick={handleNext}
-        >
-          <img
-            src="/assets/buttons/button_right.png"
-            alt="right"
-            className="w-full h-full transition hover:brightness-95 active:brightness-92"
-          />
-        </button>
-      </div>
+          {/* 이동 버튼 */}
+          <button
+            className="absolute left-0 -translate-x-1/2 top-1/2 -translate-y-1/2 w-[40px] h-[40px] cursor-pointer"
+            onClick={handlePrev}
+          >
+            <img
+              src="/assets/buttons/button_left.png"
+              alt="left"
+              className="w-full h-full transition hover:brightness-95 active:brightness-92"
+            />
+          </button>
+          <button
+            className="absolute right-0 translate-x-1/2 top-1/2 -translate-y-1/2 w-[40px] h-[40px] cursor-pointer "
+            onClick={handleNext}
+          >
+            <img
+              src="/assets/buttons/button_right.png"
+              alt="right"
+              className="w-full h-full transition hover:brightness-95 active:brightness-92"
+            />
+          </button>
+        </div>
+      )}
     </section>
   );
 };
